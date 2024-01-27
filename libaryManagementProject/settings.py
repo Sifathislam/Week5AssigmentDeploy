3ec6cd4d16efb31a3d6f009d444322a3b9b82f73
sifath islam <sifathislam790@gmail.com>
2024-01-28 05:48:35 +0600

    all work is finished for now

--- libaryManagementProject/settings.py
+++ libaryManagementProject/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
-
+CSRF_TRUSTED_ORIGINS = ['https://librarymanagementproject.onrender.com','https://*.127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
